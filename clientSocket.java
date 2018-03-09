eba9c4e158e9bf7cb6d247afafbd0d927ef8742a
zhmi1204 <zhmi1204@163.com>
2018-03-09 16:38:32 +0800

    nothing
    nothing

---
+++ clientSocket.java
@@ -0,0 +1,13 @@
+while(true){
+	try{
+		Socket socket=new Socket("127.0.0.1",9999);
+		OutputStream os=socket.getOutputStream();
+		PrintWriter print=new PrintWriter(os);
+		print.write("你好啊！服务器");
+		print.close();
+		os.close();
+	}
+	catch (IOException e){
+		e.printStackTrace();
+	}
+}
