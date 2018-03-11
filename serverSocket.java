e1786ea98b59cd6792a57c0fdbe857ece2592703
zhmi1204 <zhmi1204@163.com>
2018-03-11 21:34:11 +0800

    nothing
    nothing

--- serverSocket.java
+++ serverSocket.java
@@ -1,27 +1,31 @@
-while(true){
-	try{
-		//创建绑定到特定端口的服务端套接字
-		ServerSocket serverSocket=new ServerSocket(9999)
-		Syetem.out.println("***服务器即将启动，请等待客户端链接***");
-		//侦听并接收到此套接字的链接
-		Socket socket=serverSocket.accept();
-		//获取输出流
-		InputStream is=socket.getInputStream();
-		//将字节输入流转化为字符输入流
-		InputStreamReader isr=new InputStreamReader(is);
-		BufferReader br=new BufferReader(isr);
-		String str=null;
-		while((str=br.readLine())!=null){
-			System.out.println("服务器说：客户端发送了:" + str);
-			br.readLine();
+public class serverSocket{
+	public static void main(String[] args){
+		while(true){
+			try{
+				//创建绑定到特定端口的服务端套接字
+				ServerSocket serverSocket=new ServerSocket(9999)
+				Syetem.out.println("***服务器即将启动，请等待客户端链接***");
+				//侦听并接收到此套接字的链接
+				Socket socket=serverSocket.accept();
+				//获取输出流
+				InputStream is=socket.getInputStream();
+				//将字节输入流转化为字符输入流
+				InputStreamReader isr=new InputStreamReader(is);
+				BufferReader br=new BufferReader(isr);
+				String str=null;
+				while((str=br.readLine())!=null){
+					System.out.println("服务器说：客户端发送了:" + str);
+					br.readLine();
+				}
+				//此套接字的输入流置入流的末尾
+				socket.shutdownInput();
+				br.close();
+				isr.close();
+				is.close();
+			}
+			catch (IOException e){
+				e.printStackTrace();
+			}
 		}
-		//此套接字的输入流置入流的末尾
-		socket.shutdownInput();
-		br.close();
-		isr.close();
-		is.close();
-	}
-	catch (IOException e){
-		e.printStackTrace();
 	}
 }
