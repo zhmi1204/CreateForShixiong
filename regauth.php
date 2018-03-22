f2df8bd976e0cdf9edffd3130e080c5c914009e8
zhmi1204 <zhmi1204@163.com>
2018-03-23 00:24:22 +0800

    测试
    1、本机apache环境未解决
    2、mysql连接、入库未解决
    3、页面实时显示接收的字符串未解决

---
+++ regauth.php
@@ -0,0 +1,44 @@
+<strong><span style="font-family:KaiTi_GB2312;font-size:18px;"><?php
+    header("content-type:text/html charset=utf-8");
+     
+    //开启session
+    session_start();
+ 
+    //准备画布
+    $im=imagecreatetruecolor(50,25);
+ 
+    //准备颜料
+    $black=imagecolorallocate($im,0,0,0);
+    $gray=imagecolorallocate($im,200,200,200);
+ 
+    //背景填充
+    imagefill($im,0,0,$gray);
+ 
+    //文字居中
+    $x=(50-10*4)/2;
+    $y=(25-5)/2+5;
+ 
+    //添加干扰素
+    for($i=0;$i<50;$i++){
+        imagesetpixel($im,mt_rand(0,50),mt_rand(0,25),$black);
+    }
+ 
+    //准备文字
+    $arr=array_merge(range(0,9),range('a','z'),range('A','Z'));
+    shuffle($arr);
+    $str=implode(array_slice($arr,0,4));
+ 
+    //把$str放入session中，方便所有页面中调用
+    $_SESSION['vstr']=$str;
+ 
+    $file="../fonts/simsun.ttc";
+    imagettftext($im,15,0,$x,$y,$black,$file,$str);
+ 
+ 
+    //输出到浏览器上或保存起来
+    header("content-type:image/png");
+    imagepng($im);
+ 
+    //关闭画布
+    imagedestory($im);
+?></span></strong>
